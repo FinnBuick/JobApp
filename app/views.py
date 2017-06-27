279c618c95af6a0b58ba1665c1b74787bf88c3c5
Finn Buick <finnbuick@gmail.com>
2017-06-27 20:34:01 +1000

    initial commit

---
+++ app/views.py
@@ -0,0 +1,50 @@
+from flask import render_template, flash, redirect, request, url_for
+from app import app
+import csv
+
+@app.route('/')
+def homepage():
+    return render_template('main.html')
+
+@app.route('/dashboard/')
+def dashboard():
+    with open('testData.txt', 'r') as f:
+        reader = csv.reader(f, delimiter="\t")
+        data = list(reader)
+    return render_template('dashboard.html', data=data)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
+
+
+@app.errorhandler(405)
+def page_not_found(e):
+    return render_template('405.html')
+
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login_page():
+
+    error = None
+    try:
+        if request.method == 'POST':
+            attempted_username = request.form['username']
+            attempted_password = request.form['password']
+
+            flash(attempted_username)
+            flash(attempted_password)
+
+            if attempted_username == "admin" and attempted_password == "password":
+                return redirect(url_for('dashboard'))
+            else:
+                error = "Invalid credentials. Try Again."
+
+        flash(attempted_username)
+        return render_template('login.html', error=error)
+
+
+    except Exception as e:
+        flash(e)
+        return render_template('login.html', error=error)
